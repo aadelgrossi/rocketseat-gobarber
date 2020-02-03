cefd7b4f345b63fe45b206dadf5df800144b996e
Andre Del Grossi <andredg91@gmail.com>
2020-02-03 11:38:14 -0300

    Implement store user function and update route to POST method

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,9 @@
 import { Router } from 'express'
 import User from './app/models/User';
+import UserController from './app/controllers/UserController'
 
 const routes = new Router();
 
-// routes.get('/test', (req, res) => {
-//   return res.json( {message: "Hello World 2"} )
-// });
-
-routes.get('/', async(req,res) => {
-  const user = await User.create( 
-    { 
-      name: 'Andre Grossi', 
-      email: 'andredg91@gmail.com', 
-      password_hash: '12435485378'
-    });
-  return res.json(user);
-
-});
+routes.post('/users', UserController.store);
 
 export default routes;
