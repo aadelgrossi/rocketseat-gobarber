9e888768ef2d4925fd66a5fcf54e0bdd39da0cef
Andre Del Grossi <andredg91@gmail.com>
2020-02-03 11:10:17 -0300

    Create user model

---
+++ src/app/models/User.js
@@ -0,0 +1,17 @@
+import Sequelize, { Model } from 'sequelize'
+
+class User extends Model {
+  static init(sequelize) {
+    super.init( {
+      name: sequelize.STRING,
+      email: sequelize.STRING,
+      password_hash: sequelize.STRING,
+      provider: Sequelize.BOOLEAN,
+    },
+    {
+      sequelize,
+    });
+  }
+}
+
+export default User;
