1e1cd944bb0245ee8250d8bd512972ed2abe8cee
Andre Del Grossi <andredg91@gmail.com>
2020-02-03 21:33:46 -0300

    Rollback to vanilla ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,10 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    'airbnb-base', 'prettier'
-  ],
-  plugins: ['prettier'],
+  // extends: [
+  //   'airbnb-base', 'prettier'
+  // ],
+  //plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -16,7 +16,7 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    "prettier/prettier": "error",
+    //"prettier/prettier": "error",
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase": "off",
