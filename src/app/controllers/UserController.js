64016b90edb5e3614bf8e946d909aebaf682455a
Andre Del Grossi <andredg91@gmail.com>
2020-02-04 00:50:24 -0300

    Implement update user function with validations

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -20,8 +20,36 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
-    return res.json( { ok: true} )
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    // if user is changing email, make sure it is not in use
+    if (email && (email != user.email)) {
+      const userExists = await User.findOne( { where: { email: email } })
+      if (userExists) {
+        return res.status(400).json( { error: 'User already exists'} );
+      }
+    }
+
+    // if user provides informs his current password incorrectly (apply AND condition to make sure oldPassword was sent)
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json( { error: 'Password does not match'} );
+    } 
+
+    //if user provides same password for old and new (password unchanged)
+    if ( (await user.checkPassword(oldPassword)) == (await user.checkPassword(req.body.password)) ) {
+      return res.status(401).json( { error: 'New password must be different'} );
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider
+    });
   }
 
 }
