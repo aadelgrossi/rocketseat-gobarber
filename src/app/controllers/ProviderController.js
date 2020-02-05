cc60095bae46f79c6a125a48a17c6283e3f7677c
Andre Del Grossi <andredg91@gmail.com>
2020-02-05 16:33:51 -0300

    Search for users that are providers and select attributes

--- src/app/controllers/ProviderController.js
+++ src/app/controllers/ProviderController.js
@@ -1,8 +1,14 @@
 import User from '../models/User'
+import File from '../models/File'
 
 class ProviderController {
   async index(req, res) {
+    const providers = await User.findAll({
+      where: { provider: true },
+      attributes: ['id', 'name', 'email', 'avatar_id']
+    });
 
+    return res.json(providers);
   }
 
 }
