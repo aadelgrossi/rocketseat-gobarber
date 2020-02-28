5704e25f64bba81e7fbb9dfd5f9ecafd2bac611b
Andre Del Grossi <andredg91@gmail.com>
2020-02-28 20:13:29 -0300

    Create and populate notifications array with most recent sorting and pagination

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -9,7 +9,13 @@ class NotificationController {
       return res.status(401).json( { error: 'Notifications are available only to providers'})
     }
 
-    return res.json();
+    const notifications = await Notification.find({
+      user: req.userId,
+    })
+      .sort({ createdAt: 'desc'})
+      .limit(20);
+
+    return res.json(notifications);
   }
 }
 
