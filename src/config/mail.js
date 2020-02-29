092b5a5e906cdb01e76dbbac7523330d6dd7f673
Andre Del Grossi <andredg91@gmail.com>
2020-02-29 03:58:34 -0300

    Install nodemailer and create initial Mailtrap config for testing

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "e2461a95761f8f",
+    pass: "b8ea9d931525c6"
+  },
+  default: {
+    from: 'Go Barber <noreply@gobarber.com>',
+  }
+};
