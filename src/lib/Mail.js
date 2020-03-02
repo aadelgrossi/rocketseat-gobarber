70671a4ae26198aa1cb804bb360ddc45ec70dcbe
Andre Del Grossi <andredg91@gmail.com>
2020-03-02 00:50:55 -0300

    Install handlebars and integrations, configure views/layouts dir and initialization

--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,5 +1,8 @@
 import nodemailer from 'nodemailer';
 import mailConfig from '../config/mail';
+import exphbs from 'express-handlebar';
+import nodemailerhbs from 'nodemailer-express-handlebars';
+import { resolve } from 'path';
 
 class Mail {
   constructor() {
@@ -12,6 +15,8 @@ class Mail {
         secure, 
         auth: auth.user ? auth : null 
       });
+
+    this.configureTemplates();
   }
 
   sendMail(message) {
@@ -19,6 +24,21 @@ class Mail {
       ...mailConfig.default,
       ...message,
     })
+  }
+
+  configureTemplates() {
+    const viewPath = resolve(__dirname, "..", "app", "views", "emails")
+
+    this.transporter.use('compile', nodemailerhbs({
+      viewEngine: exphbs.create({
+        layoutsDir: resolve(viewPath, 'layouts'),
+        partialsDir: resolve(viewPath, 'partials'),
+        defaultLayout: 'default',
+        extname: '.hbs'
+      }),
+      viewPath,
+      extname: 'hbs'
+    }));
   }
 }
 
