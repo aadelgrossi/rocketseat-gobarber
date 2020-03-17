53922b10ff8aa3769154a85c6eb1954beebce34b
Andre Grossi <andredg91@gmail.com>
2020-03-17 03:49:12 -0300

    Create and import into App global styles

---
+++ frontend/src/styles/global.js
@@ -0,0 +1,49 @@
+import { createGlobalStyle } from 'styled-components';
+
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Roboto:400,700', 'sans-serif'],
+  },
+});
+
+export default createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  html, body, #root {
+    height: 100%
+  }
+
+  body {
+    -webkit-font-smoothing: antialised;
+  }
+
+  body, input, button {
+    font-family: 'Roboto', sans-serif;
+    font-size: 14px;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+`;
