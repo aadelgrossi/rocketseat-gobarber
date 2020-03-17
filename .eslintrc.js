6743f0503e3f90255b34ff9cb82415e9daaaf0c9
Andre Grossi <andredg91@gmail.com>
2020-03-17 00:21:30 -0300

    Reinstall and reconfigure prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  // extends: [
-  //   'airbnb-base', 'prettier'
-  // ],
-  //plugins: ['prettier'],
+  extends: ['airbnb-base', 'prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -15,8 +12,9 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: ['prettier'],
   rules: {
-    //"prettier/prettier": "error",
+    "prettier/prettier": "error",
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase": "off",
