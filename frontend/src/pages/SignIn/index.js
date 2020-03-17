4913ef9f63b5f2aeb052af4935782910fbbef181
Andre Grossi <andredg91@gmail.com>
2020-03-17 11:05:57 -0300

    style: Update css tou se unform styled Form

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form } from '~/pages/_layouts/auth/styles';
+import Input from '~/components/Input'
 import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input type="email" placeholder="Seu email" />
-        <input type="password" placeholder="Senha" />
+      <Form onSubmit={handleSubmit}>
+        <Input type="email" placeholder="Seu email" />
+        <Input type="password" placeholder="Senha" />
 
         <button type="submit">Acessar</button>
         <Link to="/signup">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
