748200f94e7ac2e74a4884a74d47817fab436225
Andre Grossi <andredg91@gmail.com>
2020-03-17 10:38:15 -0300

    refactor: Change imports to use ~ as root path config dir

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
 
-import SignUp from '../pages/SignUp';
-import SignIn from '../pages/SignIn';
-import Dashboard from '../pages/Dashboard';
-import Profile from '../pages/Profile';
+import SignUp from '~/pages/SignUp';
+import SignIn from '~/pages/SignIn';
+import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
 
 export default function Routes() {
   return (
