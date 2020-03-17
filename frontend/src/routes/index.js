a89bef4af25d693494ad4ab9546d0e12b238d2f7
Andre Grossi <andredg91@gmail.com>
2020-03-17 03:03:49 -0300

    Add pages to routes, configure history, add history and router components to App.js

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,10 +1,18 @@
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import SignUp from '../pages/SignUp';
+import SignIn from '../pages/SignIn';
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path='/' exact component={} />
+      <Route path="/" exact component={SignIn} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} />
     </Switch>
   );
 }
