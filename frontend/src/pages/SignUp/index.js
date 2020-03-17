6c67457da6ebeba7d6cd7d58732f535a75d99aed
Andre Grossi <andredg91@gmail.com>
2020-03-17 10:34:54 -0300

    style: Add and style fields for SignIn/SignUp forms

--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input placeholder="Seu nome completo" />
+        <input type="email" placeholder="Seu email" />
+
+        <input type="password" placeholder="Senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/">Já possuo conta</Link>
+      </form>
+    </>
+  );
 }
