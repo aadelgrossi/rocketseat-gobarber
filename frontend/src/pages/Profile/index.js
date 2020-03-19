5cdf27b5360b63f9607bd260c4223c300ebeca91
Andre Grossi <andredg91@gmail.com>
2020-03-19 05:00:42 -0300

    feat(user): Implement update user saga and actions
    undefined

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import Input from '~/components/Input';
 import { Container } from './styles';
 import { Form } from '~/pages/_layouts/auth/styles';
 
-// import { Container } from './styles';
-
 export default function Profile() {
+  const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
 
   return (
     <Container>
-      <Form initialData={profile}>
+      <Form initialData={profile} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu email" />
         <hr />
