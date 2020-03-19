6bb34901b98b6452551e90c0c0cdf79c0847c025
Andre Grossi <andredg91@gmail.com>
2020-03-19 07:15:04 -0300

    style(dashboard): Create base Dashboard and styles

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { Container, Appointment } from './styles';
 
 export default function Dashboard() {
-  return <div />;
+  const name = 'Andre Grossi';
+
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 de maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Appointment past>
+          <strong>08:00</strong>
+          <span>{name}</span>
+        </Appointment>
+        <Appointment available>
+          <strong>09:00</strong>
+          <span>{name}</span>
+        </Appointment>
+        <Appointment>
+          <strong>10:00</strong>
+          <span>{name}</span>
+        </Appointment>
+        <Appointment>
+          <strong>11:00</strong>
+          <span>{name}</span>
+        </Appointment>
+      </ul>
+    </Container>
+  );
 }
